d68304aa5e4b2efd4f9614ea449260cf413b3795
uziiii152 <uzairqaseem162@gmail.com>
2024-12-17 18:10:36 +0500

    refresh token updated

--- src/controller/user.controllers.js
+++ src/controller/user.controllers.js
@@ -3,7 +3,7 @@ import { ApiError } from "../utils/apiError.js";
 import { User } from "../models/user.models.js";
 import { uploadOnCloudinary, deleteFromCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/apihandler.js";
-
+import jwt from "jsonwebtoken"
 
 const generateAccessAndRefereshTokens = async(userId) =>{
     try {
@@ -137,8 +137,54 @@ return res
         "user logged in successfully"
     ))
 })
+const logoutUser = asyncHandler(async (req, res) => {
+    await User.findByIdAndUpdate(
+        // req.user._id need to come back after middlerware video this todo for us
+    )
+}) 
+const refreshAccessToken = asyncHandler( async (req,res) => {
+  const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
+  if (!incomingRefreshToken) {
+    throw new ApiError(401, "refresh token is required")
+  }  
+  try {
+    const decodedToken = jwt.verify(
+        incomingRefreshToken,
+        process.env.REFRESH_TTOKEN_SECRET
+    )
+    const user = await User.findById(decodedToken?._id)
+   if (!user) {
+    throw new ApiError(401, "Invalid refresh token")
+   } 
+if (incomingRefreshToken !== user?.refreshToken) {
+    throw new ApiError(401, "Invalid refresh token")
+}
+
+const options = {
+   httpOnly : true,
+   secure: process.env.NODE_ENV === "production" 
+}
+const {accessToken,refreshToken: newRefreshToken}= await generateAccessAndRefereshTokens(user._id)
+return res
+.status(200)
+.cookie("accessToken", accessToken,options)
+.cookie("refreshToken",newRefreshToken,options)  
+.json(new ApiResponse(
+    200,
+    {
+        accessToken,
+        refreshToken: newRefreshToken
+    },
+    "Access token refreshed successfully"
+))
+
+} catch (error) {
+    throw new ApiError(500, "Something went wrong while refreshing access token")
+  }
+})
 
 export { 
     registerUser,
-    loginUser
+    loginUser,
+    refreshAccessToken
 };
