0bf1e0fd56f33cbbc48294cbfafc06308cd8ce12
unknown <uzairqaseem162@gmail.com>
2024-12-26 22:47:04 +0500

    corrected some errors & imported getCurrentUser in user.routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,18 @@
 
 import express from 'express';
 import {upload} from "../middlewares/multer.middlewares.js"
-import {registerUser,logoutUser,loginUser,refreshAccessToken,changeCurrentPassword,getUserChannelProfile,updateAccountDetails,updateUserAvatar,updateUserCoverImage,getWatchHistory} from '../controller/user.controllers.js'; 
-import verifyJWT from "../middlewares/auth.middlewares.js"
+import {registerUser,
+    logoutUser,
+    loginUser,
+    refreshAccessToken,
+    changeCurrentPassword,
+    getUserChannelProfile,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage,
+    getWatchHistory,
+    getCurrentUser} from '../controller/user.controllers.js'; 
+import {verifyJWT} from "../middlewares/auth.middlewares.js"
 
 
 
@@ -23,12 +33,12 @@ userRouter.route('/refresh-token').post(refreshAccessToken)
 //secured routes
 userRouter.route('/logout').post(verifyJWT,logoutUser)
 userRouter.route('/change-password').post(verifyJWT,changeCurrentPassword)
-router.route('/current-user').get(verifyJWT,getCurrentUser)
-router.route('/c/:username').get(verifyJWT,getUserChannelProfile)
-router.route('/update-account').patch(verifyJWT,updateAccountDetails)
-router.route('/avatar').patch(verifyJWT, upload.single('avatar'),updateUserAvatar)
-router.route('/cover-Image').patch(verifyJWT, upload.single('coverImage'),updateUserCoverImage)
-router.route('/history').get(verifyJWT,getWatchHistory)
+userRouter.route('/current-user').get(verifyJWT,getCurrentUser)
+userRouter.route('/c/:username').get(verifyJWT,getUserChannelProfile)
+userRouter.route('/update-account').patch(verifyJWT,updateAccountDetails)
+userRouter.route('/avatar').patch(verifyJWT, upload.single('avatar'),updateUserAvatar)
+userRouter.route('/cover-Image').patch(verifyJWT, upload.single('coverImage'),updateUserCoverImage)
+userRouter.route('/history').get(verifyJWT,getWatchHistory)
 
 
 export default userRouter; 
